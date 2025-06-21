d0a80d930b57b3fbe31ec719bb2f8dc39750b844
andre10dias <andre10dias@ymail.com>
2025-06-21 20:09:22 -0300

    Autenticação via Filtro

---
+++ sbootexp-security/src/main/java/com/github/andre10dias/sbootexp_security/config/CustomFilter.java
@@ -0,0 +1,41 @@
+package com.github.andre10dias.sbootexp_security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class CustomFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String secretHeader = request.getHeader("x-secret");
+
+        if (secretHeader != null && secretHeader.equals("secr3t")) {
+            Authentication auth = new UsernamePasswordAuthenticationToken(
+                    "Muito secreto",
+                    null,
+                    List.of(new SimpleGrantedAuthority("ADMIN"))
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
