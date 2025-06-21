12ed9e6f55c8b7402a6e8da8435eb84d78aafc73
andre10dias <andre10dias@ymail.com>
2025-06-21 17:38:25 -0300

    Configuração básica do Spring Security

---
+++ sbootexp-security/src/main/java/com/github/andre10dias/sbootexp_security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.github.andre10dias.sbootexp_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception {
+        return http
+                .authorizeHttpRequests(customizer -> {
+                    customizer
+                            .requestMatchers("/public/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
