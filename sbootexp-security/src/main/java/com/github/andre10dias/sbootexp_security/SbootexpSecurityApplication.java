abf3d9c64be8ad414a76bdeb843a070f6d22cd89
andre10dias <andre10dias@ymail.com>
2025-06-21 13:25:55 -0300

    Projeto inicial

---
+++ sbootexp-security/src/main/java/com/github/andre10dias/sbootexp_security/SbootexpSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.github.andre10dias.sbootexp_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootexpSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootexpSecurityApplication.class, args);
+	}
+
+}
